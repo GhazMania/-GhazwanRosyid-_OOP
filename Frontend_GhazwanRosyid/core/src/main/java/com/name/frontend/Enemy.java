aaa2cad7c1f2075fdfc258a8edaa59a3e7ceca46
GhazMania <ghazwanrosyid.mr@gmail.com>
2026-09-04 06:50:08 +0700

    GhazwanRosyid - init

---
+++ Frontend_GhazwanRosyid/core/src/main/java/com/name/frontend/Enemy.java
@@ -0,0 +1,12 @@
+package com.name.frontend;
+
+public class Enemy {
+    String name;
+    int hp;
+    int maxHp;
+    public Enemy(String name, int hp){
+        this.name = "Cirno";
+        this.hp = 100;
+        this.maxHp = 15;
+    }
+}
---
+++ Frontend_GhazwanRosyid/core/src/main/java/com/name/frontend/Player.java
@@ -0,0 +1,15 @@
+package com.name.frontend;
+
+public class Player {
+    String name;
+    int hp;
+    int maxHp;
+    int power;
+    int spellCards;
+    public Player(String name, int hp, int power, int spellCards){
+        this.name = "Reimu Hakurei";
+        this.hp = 100;
+        this.power = 15;
+        this.spellCards = 3;
+    }
+}
