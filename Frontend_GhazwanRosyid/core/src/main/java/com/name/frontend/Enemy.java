6a3e33dbc60204322395660804a30b77a54f5384
GhazMania <ghazwanrosyid.mr@gmail.com>
2026-09-04 10:41:40 +0700

    CS1 - Brocacho

--- Frontend_GhazwanRosyid/core/src/main/java/com/name/frontend/Enemy.java
+++ Frontend_GhazwanRosyid/core/src/main/java/com/name/frontend/Enemy.java
@@ -9,4 +9,29 @@ public class Enemy {
         this.hp = 100;
         this.maxHp = 15;
     }
+    public void takeDamage(int damage) {
+        this.hp = hp-damage;
+        // 2. HP must not become negative.
+        if(this.hp<0){
+            this.hp=0;
+            System.out.println("[" +this.name +"] was defeated\n");
+        }else {
+            System.out.println("["+ this.name +"] took " + damage + " damage! HP: ["+ this.hp +"/"+this.maxHp+"]\n");
+        }
+    }
+
+    public void attack(Player player, int damage) {
+        // 1. Display information that the Enemy is attacking the Player, in the format: [EnemyName] unleashes bullet barrage on [PlayerName]!
+        System.out.println("[" +this.name+ "] unleashes bullet barrage on [" +player.name+ "]!\n");
+        // 2. Call the Player's takeDamage() method using the given damage.
+        player.takeDamage(damage);
+    }
+    public boolean isAlive() {
+        // 1. Return true if hp > 0, and false otherwise
+        if(this.hp>0){
+            return true;
+        }else
+            return false;
+    }
+
 }
--- Frontend_GhazwanRosyid/core/src/main/java/com/name/frontend/Player.java
+++ Frontend_GhazwanRosyid/core/src/main/java/com/name/frontend/Player.java
@@ -12,4 +12,34 @@ public class Player {
         this.power = 15;
         this.spellCards = 3;
     }
+    public void takeDamage(int damage) {
+        // 1. Reduce hp by the damage value.
+        this.hp = hp-damage;
+        // 2. HP must not become negative.
+        if(this.hp<0){
+            this.hp=0;
+            System.out.println("You lose\n");
+        }else {
+            // 3. If HP is still greater than 0, display the remaining HP in the format: [PlayerName] took [damage] damage! Remaining HP: [hp]
+            System.out.println("["+ this.name +"] took " + damage + " damage! Remaining HP: ["+ this.hp +"]\n");
+        }
+        // 4. If HP reaches 0, display a message that the Player has been defeated.
+    }
+    public void shoot(Enemy target) {
+        // 1. Create an int named damage, calculated by adding 10 to power.
+        int damage = this.power + 10;
+
+        // 2. Display information that the Player is shooting the Enemy, in the format: [name] shoots [TargetName] dealing [damage] DMG!
+        System.out.println("[" +this.name+ "] shoots [" + target.name+ "] dealing [" +damage+ "] DMG!");
+        // 3. Call the Enemy object's takeDamage() method.
+        target.takeDamage(damage);
+    }
+
+    public boolean isAlive() {
+        // 1. Return true if hp > 0, and false otherwise
+        if(this.hp>0){
+            return true;
+        }else
+            return false;
+    }
 }
